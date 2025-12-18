1d9739f5b49f4f2027fd693800f0dd8bba2aaa20
Viaasyafrina16 <syafrinavia@gmail.com>
2025-12-18 09:11:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/localrest/modeldata/DataSiswa.kt
@@ -0,0 +1,26 @@
+package com.example.localrest.modeldata
+
+@Serializable
+data class DataSiswa(
+    val id : Int,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
+
+data class UIStateSiswa(
+    val detailSiswa : DetailSiswa = DetailSiswa(),
+    val isEntryValid : Boolean = false
+)
+
+data class DetailSiswa(
+    val id : Int = 0,
+    val nama : String = "",
+    val alamat : String = "",
+    val telpon: String = ""
+)
+
+fun DetailSiswa.toDataSiswa() : DataSiswa = DataSiswa(
+    id = id,
+    
+)
