25277db683fa8c4c8f49f05a691ae5ed0e6d19e2
Viaasyafrina16 <syafrinavia@gmail.com>
2025-12-18 15:19:47 +0700

    tidak meangktifkan GET dan PUSH

--- app/src/main/java/com/example/localrest/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/localrest/apiservice/ServiceApiSiswa.kt
@@ -1,5 +1,6 @@
 package com.example.localrest.apiservice
 
+import com.example.localrest.modeldata.DataSiswa
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -11,3 +12,9 @@ interface ServiceApiSiswa {
     @POST("insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
+
+//    @GET("baca1Teman.php/{id}")
+//    suspend fun getStasusSiswa(@Query("id") id: Int): DataSiswa
+
+//    @PUT("editTM.php/{id}")
+//    suspend fun editStatusSiswa(@Qyery("id") id: Int): DataSiswa
