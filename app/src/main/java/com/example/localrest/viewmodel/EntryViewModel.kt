7e492ffc5874b444aff3740e1db9072a9c9bf285
Viaasyafrina16 <syafrinavia@gmail.com>
2025-12-25 12:58:58 +0700

    implementasi EditViewModel dengan pengambilan data otomatis berdasarkan ID

--- app/src/main/java/com/example/localrest/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/localrest/viewmodel/EntryViewModel.kt
@@ -4,20 +4,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.example.localrest.Repositpory.RepositoryDataSiswa
+import com.example.localrest.Repository.RepositoryDataSiswa
 import com.example.localrest.modeldata.DetailSiswa
 import com.example.localrest.modeldata.UIStateSiswa
 import com.example.localrest.modeldata.toDataSiswa
 import retrofit2.Response
-
-class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
-    ViewModel() {
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
-    /* Fungsi untuk memvalidasi input */
-    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ):
-            Boolean {
+    //Fungsi untuk memvalidasi input
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
         return with(uiState) {
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
@@ -25,15 +22,14 @@ class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa):
     //Fungsi untuk menangani saat ada perubahan pada text input
     fun updateUiState(detailSiswa: DetailSiswa) {
         uiStateSiswa =
-            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput
-                (detailSiswa))
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
 
     /* Fungsi untuk menyimpan data yang di-entry */
     suspend fun addSiswa() {
         if (validasiInput()) {
-            val sip: Response<Void> =repositoryDataSiswa.postDataSiswa(uiStateSiswa
-                .detailSiswa.toDataSiswa())
+            val sip:Response<Void> =repositoryDataSiswa.postDataSiswa(uiStateSiswa.detailSiswa
+                .toDataSiswa())
             if (sip.isSuccessful){
                 println("Sukses Tambah Data : ${sip.message()}")
             }else{
