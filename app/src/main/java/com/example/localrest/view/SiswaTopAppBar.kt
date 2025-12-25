ed7a57995adbb632f76fc169c67b6b26076b97d8
Viaasyafrina16 <syafrinavia@gmail.com>
2025-12-25 12:39:52 +0700

    implementasi RepositoryDataSiswa dan JaringanRepositoryDataSiswa

--- app/src/main/java/com/example/localrest/view/SiswaTopAppBar.kt
+++ app/src/main/java/com/example/localrest/view/SiswaTopAppBar.kt
@@ -13,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.example.localrest.R
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SiswaTopAppBar(
